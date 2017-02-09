e8b412632f6387ef681526bef9d158f5aeb21198
SanketJoshi03 <sanketjoshir@gmail.com>
2017-02-09 13:27:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cabmaxlvn01/dayone/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.cabmaxlvn01.dayone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.security.PrivateKey;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpload(){
+
+
+        int i= 1+1;
+    }
+}
+
